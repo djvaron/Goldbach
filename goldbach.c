493b5f66a409f5793aff933820fc84004c6e1403
Ada Shaw <ayshaw@g.harvard.edu>
2018-04-01 11:52:24 -0400

    asdf

--- goldbach.c
+++ goldbach.c
@@ -11,7 +11,8 @@ void isprime(int n)
     // finally be false if i is Not a prime, else true.
     bool prime[n+1];
     memset(prime, true, sizeof(prime));
-    smallest_square=floor(pow(n,0.5))
+    int smallest_square;
+    smallest_square=floor(pow(n,0.5));
     for (int p=2; p<=smallest_square; p++)
     {
         // If prime[p] is not changed, then it is a prime
@@ -22,29 +23,29 @@ void isprime(int n)
                 prime[i] = false;
         }
     }
-
+    
 }
 
 
 
 int main(int argc, char** argv) {
-
-    int lower, upper, count;    
+    
+    int lower, upper, count;
     lower = atoi(argv[1]);
     upper = atoi(argv[2]);
-
+    
     for (int n = lower; n <= upper; n += 2) {
         count = 0;
-	for (int i = 1; i <= n / 2; i++) {
+        for (int i = 1; i <= n / 2; i++) {
             if (isprime(i) && isprime(n - i)) {
                 printf("TRUE %d = %d + %d\n", n, i, n - i);
                 count = 1;
-		break;
-	    }
+                break;
+            }
         }
         if (count == 0) {
-	    printf("FALSE %d", n);
-	}
+            printf("FALSE %d", n);
+        }
     }
     return 0;
-}          
+}
