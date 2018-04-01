54e686de5d0b4391d3d9f003b2b2b499391c5341
Ada Shaw <ayshaw@g.harvard.edu>
2018-04-01 11:48:48 -0400

    asdf

---
+++ goldbach.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include "math.h"
+using namespace std;
+
+void isprime(int n)
+{
+    // Create a boolean array "prime[0..n]" and initialize
+    // all entries it as true. A value in prime[i] will
+    // finally be false if i is Not a prime, else true.
+    bool prime[n+1];
+    memset(prime, true, sizeof(prime));
+    smallest_square=floor(pow(n,0.5))
+    for (int p=2; p<=smallest_square; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*2; i<=n; i += p)
+                prime[i] = false;
+        }
+    }
+
+}
+
+
+
+int main(int argc, char** argv) {
+
+    int lower, upper, count;    
+    lower = atoi(argv[1]);
+    upper = atoi(argv[2]);
+
+    for (int n = lower; n <= upper; n += 2) {
+        count = 0;
+	for (int i = 1; i <= n / 2; i++) {
+            if (isprime(i) && isprime(n - i)) {
+                printf("TRUE %d = %d + %d\n", n, i, n - i);
+                count = 1;
+		break;
+	    }
+        }
+        if (count == 0) {
+	    printf("FALSE %d", n);
+	}
+    }
+    return 0;
+}          
