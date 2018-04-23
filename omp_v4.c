eb9fad0cff52e91205920b621447e296464c6ef6
Ada Shaw <ayshaw@g.harvard.edu>
2018-04-23 13:43:01 -0400

    working omp

--- omp_v4.c
+++ omp_v4.c
@@ -9,10 +9,12 @@ int * sieve(int limit){
     int *primes;
 
     primes = malloc(sizeof(int) * limit);
- for (i = 2; i < limit; i++)
+#pragma omp parallel for schedule(dynamic) 
+for (i = 2; i < limit; i++)
         primes[i] = 1;
 int p;
 int n = floor(pow(limit,0.5));
+#pragma omp parallel for schedule(dynamic)
 for (p=2; p<=n; p++)
 
         // If prime[p] is not changed, then it is a prime
@@ -20,6 +22,7 @@ for (p=2; p<=n; p++)
         {
             // Update all multiples of p
       int i;
+#pragma omp parallel for schedule(dynamic)
       for ( i=p*2; i<=limit; i += p)
                 primes[i] = 0;
         }
@@ -48,10 +51,10 @@ int main(int argc, char** argv) {
     lower = atoi(argv[1]);
     upper = atoi(argv[2]);
 
-    clock_t begin = clock();
-    
+  //  clock_t begin = clock();
+    double begin = omp_get_wtime();
     int * primes = sieve(upper);    
-   
+   #pragma omp parallel for schedule(dynamic)
     for (n = lower; n <= upper; n += 2) {
         count = 0;
 	for (i = 2; i <= n/2; i++) {
@@ -65,9 +68,9 @@ int main(int argc, char** argv) {
 	    printf("FALSE %d", n);
 	}
     }
-   
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;    
+   double time_spent = omp_get_wtime() - begin;
+//    clock_t end = clock();
+//    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;    
     printf("time spent: %g seconds\n",time_spent);
 
     free(primes), primes = NULL;
