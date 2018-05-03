6e92bf10c0a342efc808f7afd390793da4e7075e
ayshaw <ayshaw@g.harvard.edu>
2018-05-03 14:08:23 +0000

    acc code works but isn't as fast as i would like it

--- acc_gold.c
+++ acc_gold.c
@@ -20,12 +20,12 @@ int * sieve(int limit){
 {
 #pragma acc region
 {
-#pragma acc loop independent gang(16), vector(512)   
+#pragma acc loop independent gang(50), vector(256) 
 for (i = 2; i < n; i++)
      // If prime[i] is not changed, then it is a prime
      if (primes[i]) {
          // Update all multiples of i
-	  #pragma acc loop independent gang(16), vector(512)
+	  #pragma acc loop independent gang(100), vector(2048)
          for ( j = 2*i; j < limit; j += i)
              primes[j] = 0;
      }
@@ -49,26 +49,32 @@ int main(int argc, char** argv) {
 
   //  clock_t begin = clock();
     double begin = omp_get_wtime();
-    int * primes = sieve(upper);    
-   #pragma omp parallel for schedule(dynamic)
+    int * primes = sieve(upper);
+    int jar = 0;    
+#pragma acc parallel copyin(primes[0:upper]) private(jar)
+{
+#pragma acc loop 
     for (n = lower; n <= upper; n += 2) {
         count = 0;
 	for (i = 2; i <= n/2; i++) {
             if (primes[i] && primes[n-i]) {
-               // printf("TRUE %d = %d + %d\n", n, i, n-i);
+  //              printf("TRUE %d = %d + %d\n", n, i, n-i);
                 count = 1;
 		break;
 	    }
         }
         if (count == 0) {
-	    printf("FALSE %d", n);
+//	    printf("FALSE %d", n);
+	    jar = 1;
 	}
     }
+}
+
    double time_spent = omp_get_wtime() - begin;
 //    clock_t end = clock();
 //    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;    
     printf("time spent: %g seconds\n",time_spent);
-
+    printf("if jar =  0, goldbach's conjecture stands.\n jar = %d\n",jar);
     free(primes), primes = NULL;
 
     return 0;
