82b1abd17ed88b5efd9dcd7bb4ba578578e57171
Ada Shaw <ayshaw@g.harvard.edu>
2018-05-04 17:22:28 -0400

    added images

--- plots.R
+++ plots.R
@@ -71,7 +71,7 @@ parallelTime <- c(2.12186,
                   2.83341,
                   9.38933,
                   12.3877,
-                  11.1563)
+                  NA)
 serialTime <- c(4.20E-05,
                 4.40E-05,
                 0.000152,
@@ -80,8 +80,7 @@ serialTime <- c(4.20E-05,
                 0.256998,
                 3.79028,
                 42.7085,
-                61.3641,
-                NA)
+                61.3641)
 unoptimizedParallel <- c(2.03827,
                          1.95644,
                          1.88567,
@@ -90,15 +89,32 @@ unoptimizedParallel <- c(2.03827,
                          2.33071,
                          4.79394,
                          33.8034,
-                         53.9939,
-                         NA)
-A <- c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+                         53.9939)
+A <- c(2, 3, 4, 5, 6, 7, 8, 9, 10)
 problemSize <- c(10^A)
 plot(problemSize,serialTime,type = 'p', log ='x', col = "black", pch = 16, 
      ylab = "Execution Time [s]", xlab = "Problem Size", main = "Execution Times versus Problem Size",axes = F, ylim = c(0,100))
-points(problemSize,parallelTime,type = 'p', col = "blue", pch =17)
-points(problemSize,unoptimizedParallel, type = "p", col ="red", pch =15)
-axis(1,at = problemSize, labels = NULL)
-axis(2,at = seq(0,100,by= 10),labels = NULL)
+points(problemSize, parallelTime,type = 'p', col = "blue", pch =17)
+points(problemSize, unoptimizedParallel, type = "p", col ="red", pch =15)
+axis(1, at = problemSize, labels = NULL)
+axis(2, at = seq(0,100,by= 10),labels = NULL)
 legend("topleft", legend = c("serial","optimized parallel", "parallel"), pch = c(16,17,15), bty = "n", col = c('black' ,'blue','red'))
 dev.off()
+##########################################profiling#################################################
+rm(list=ls())
+png("profiling.png",width = 6, height = 6, units = "in", res = 300)
+problemSize <- c(6, 7, 8, 9, 10)
+Sieve <- c(25.19,33.59,56.13 ,60.27,60.02)*.01
+totalTime <- c(.04,0.54,10.36,140.05,201.47)
+Main <- 1 - Sieve
+df <- data.frame(problemSize, Sieve, Main)
+library(reshape2)
+DF1 <- melt(df, id.var="problemSize")
+library(ggplot2)
+ggplot(DF1, aes(x = problemSize, y = value,fill=variable)) +
+  geom_bar(stat='identity')+
+  ggtitle(TeX("Function Percentage with $10^n$"))+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylab("%")+xlab("n")+ theme_classic()
+dev.off()
+
