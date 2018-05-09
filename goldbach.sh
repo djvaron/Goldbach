55b0a35daa897d4a5ea1f8283f99e03741697796
Ada Shaw <ayshaw@g.harvard.edu>
2018-05-09 02:39:50 -0400

    some changes to omp

--- goldbach.sh
+++ goldbach.sh
@@ -4,12 +4,12 @@
 #SBATCH -t 0-01:00              # Runtime in D-HH:MM
 #SBATCH -p huce_intel           # Partition to submit to
 #SBATCH --mem=64000            # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o threads_32_%j.out           # File to which STDOUT will be written
-#SBATCH -e threads_32_%j.err           # File to which STDERR will be written
+#SBATCH -o n10_%j.out           # File to which STDOUT will be written
+#SBATCH -e n10_%j.err           # File to which STDERR will be written
 #SBATCH --mail-type=END         # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=ayshaw@g.harvard.edu      # Email to which notifications will be sent
 
 export OMP_NUM_THREADS=32
 export OMP_STACKSIZE="512M"
 
-./omp_goldbach 4 1000000000
+./omp_goldbach_v2 4 10000000000
